5b80f2a4610746c43d561e20ed3e0d2f097fbd0b
Andrew <andrew.logan@hotmail.com>
2023-06-29 21:35:32 +1200

    v1

--- Program.cs
+++ Program.cs
@@ -1,18 +1,33 @@
 using lingoSimApi;
 using MongoDB.Driver;
 using System.Configuration;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+string keyVaultURL = "https://lingosimsecrets.vault.azure.net/";
+string keyVaultSecretName = "MongoString";
+
+var client = new SecretClient(new Uri(keyVaultURL),
+    new DefaultAzureCredential());
+
+var secret = await client.GetSecretAsync(keyVaultSecretName);
+
 builder.Services.AddControllers();
+
+string connectionString = secret.Value.Value; // Get the secret value
 
 builder.Services.AddSingleton<IMongoClient>(sp =>
 {
-    var connectionString = builder.Configuration["MongoString"];
-    return new MongoClient(connectionString);
+    var settings = MongoClientSettings.FromConnectionString(connectionString);
+    return new MongoClient(settings);
 });
+
 
 builder.Services.AddScoped<IMongoDatabase>(sp =>
 {

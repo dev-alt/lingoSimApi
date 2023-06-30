f52fcd9497af9111a20f6e1a863af2d074cd921b
Andrew <andrew.logan@hotmail.com>
2023-06-30 16:41:59 +1200

    v

--- Program.cs
+++ Program.cs
@@ -10,8 +10,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 
-string keyVaultURL = "https://lingosimsecrets.vault.azure.net/";
-string keyVaultSecretName = "MongoString";
 
 var client = new SecretClient(new Uri(keyVaultURL),
     new DefaultAzureCredential());
